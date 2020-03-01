19be018d6178fdbbb4112669ba9437548542e2c3
reifocS <56202201+reifocS@users.noreply.github.com>
2020-03-01 19:24:10 +0100

    Update conway_sequence.py

--- Codingame_puzzles-master/Solutions/General (boucles, conditions, structures de données)/conway_sequence/conway_sequence.py
+++ Codingame_puzzles-master/Solutions/General (boucles, conditions, structures de données)/conway_sequence/conway_sequence.py
@@ -1,4 +1,6 @@
-#output chose line of conway sequence
+# In mathematics, the look-and-say sequence is the sequence of integers beginning as follows:
+# 1, 11, 21, 1211, 111221, 312211, 13112221, 1113213211, ...
+# This programs output the chosen line of the sequence 
 
 def next_sequence(line):
     actual = line[0]
